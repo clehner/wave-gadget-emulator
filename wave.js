ff364e2bd39d8ac38650f0663b71f31b2911deac
Charles Lehner <celehner1@gmail.com>
2010-07-24 12:38:32 -0400

    Updated wave.js

--- wave.js
+++ wave.js
@@ -21,58 +21,61 @@ wave.privateState_ = null;
 wave.privateStateCallback_ = new wave.Callback(null);
 wave.mode_ = null;
 wave.modeCallback_ = new wave.Callback(null);
-wave.inWaveContainer_ = false;wave.util = wave.util || {};
-wave.util.SPACES_ = "                                                 ";
-wave.util.toSpaces_ = function(b) {
-  return wave.util.SPACES_.substring(0, b * 2)
-};
-wave.util.isArray_ = function(b) {
-  try {
-    return b && typeof b.length == "number"
-  }catch(c) {
-    return false
+wave.inWaveContainer_ = false;gadgets.window = gadgets.window || {};
+(function() {
+  function b(e, d) {
+    var g = window.getComputedStyle(e, "");
+    g = g.getPropertyValue(d);
+    g.match(/^([0-9]+)/);
+    return parseInt(RegExp.$1, 10)
   }
-};
-wave.util.printJson = function(b, c, d) {
-  if(!b || typeof b.valueOf() != "object") {
-    if(typeof b == "string") {
-      return"'" + b + "'"
-    }else {
-      if(b instanceof Function) {
-        return"[function]"
+  var c;
+  gadgets.window.adjustWidth = function(e) {
+    var d = parseInt(e, 10);
+    e = false;
+    if(isNaN(d)) {
+      e = true;
+      var g = gadgets.window.getViewportDimensions().width, f = document.body, h = document.documentElement;
+      if(document.compatMode === "CSS1Compat" && h.scrollWidth) {
+        d = h.scrollWidth !== g ? h.scrollWidth : h.offsetWidth
+      }else {
+        if(navigator.userAgent.indexOf("AppleWebKit") >= 0) {
+          g = 0;
+          for(f = [document.body];f.length > 0;) {
+            h = f.shift();
+            h = h.childNodes;
+            for(d = 0;d < h.length;d++) {
+              var i = h[d];
+              if(typeof i.offsetLeft !== "undefined" && typeof i.scrollWidth !== "undefined") {
+                var j = i.offsetLeft + i.scrollWidth + b(i, "margin-right");
+                g = Math.max(g, j)
+              }
+              f.push(i)
+            }
+          }
+          d = g + b(document.body, "border-right") + b(document.body, "margin-right") + b(document.body, "padding-right")
+        }else {
+          if(f && h) {
+            d = h.scrollWidth;
+            i = h.offsetWidth;
+            if(h.clientWidth !== i) {
+              d = f.scrollWidth;
+              i = f.offsetWidth
+            }
+            d = d > g ? d > i ? d : i : d < i ? d : i
+          }
+        }
       }
     }
-    return"" + b
-  }
-  var e = [], g = wave.util.isArray_(b), f = g ? "[]" : "{}", h = c ? "\n" : "", k = c ? " " : "", l = 0;
-  d = d || 1;
-  c || (d = 0);
-  e.push(f.charAt(0));
-  for(var j in b) {
-    if(!j.match(/___$/)) {
-      var i;
-      i = j;
-      var m = b[i];
-      l++ > 0 && e.push(", ");
-      if(!g) {
-        e.push(h);
-        e.push(wave.util.toSpaces_(d));
-        e.push(i + ": ");
-        e.push(k)
-      }
-      e.push(wave.util.printJson(m, c, d + 1))
+    if(d !== c && !isNaN(d) && !(e && d === 0)) {
+      c = d;
+      gadgets.rpc.call(null, "setIframeWidth", null, d)
     }
   }
-  if(!g) {
-    e.push(h);
-    e.push(wave.util.toSpaces_(d - 1))
-  }
-  e.push(f.charAt(1));
-  return e.join("")
-};wave.Participant = function(b, c, d) {
+})();wave.Participant = function(b, c, e) {
   this.id_ = b || "";
   this.displayName_ = c || "";
-  this.thumbnailUrl_ = d || ""
+  this.thumbnailUrl_ = e || ""
 };
 wave.Participant.prototype.getId = function() {
   return this.id_
@@ -104,9 +107,9 @@ a.getKeys = function() {
   var b = [];
   for(var c in this.state_) {
     if(!c.match(/___$/)) {
-      var d;
-      d = c;
-      b.push(d)
+      var e;
+      e = c;
+      b.push(e)
     }
   }
   return b
@@ -115,17 +118,17 @@ a.submitDelta = function(b) {
   gadgets.rpc.call(null, this.rpc_, null, b)
 };
 a.submitValue = function(b, c) {
-  var d = {};
-  d[b] = c;
-  this.submitDelta(d)
+  var e = {};
+  e[b] = c;
+  this.submitDelta(e)
 };
 a.reset = function() {
   var b = {};
   for(var c in this.state_) {
     if(!c.match(/___$/)) {
-      var d;
-      d = c;
-      b[d] = null
+      var e;
+      e = c;
+      b[e] = null
     }
   }
   this.submitDelta(b)
@@ -133,8 +136,112 @@ a.reset = function() {
 a.setState_ = function(b) {
   this.state_ = b || {}
 };
+a.calculateDelta_ = function(b) {
+  var c = {};
+  for(var e in b) {
+    if(!e.match(/___$/)) {
+      var d;
+      d = e;
+      var g = this.state_.hasOwnProperty(d);
+      if(!g || this.state_[d] != b[d]) {
+        c[d] = b[d]
+      }
+    }
+  }
+  for(var f in this.state_) {
+    if(!f.match(/___$/)) {
+      d = f;
+      b.hasOwnProperty(d) || (c[d] = null)
+    }
+  }
+  return c
+};
+a.applyDelta_ = function(b) {
+  this.state_ = this.state_ || {};
+  for(var c in b) {
+    if(!c.match(/___$/)) {
+      var e;
+      e = c;
+      if(b[e] != null) {
+        this.state_[e] = b[e]
+      }else {
+        delete this.state_[e]
+      }
+    }
+  }
+};
 a.toString = function() {
   return wave.util.printJson(this.state_, true)
+};var tamings___ = tamings___ || [], caja___, ___;
+tamings___.push(function(b) {
+  function c(e, d) {
+    var g = {apply:___.markFuncFreeze(function(f, h) {
+      return ___.callPub(e, "apply", [d, h])
+    })};
+    return new wave.Callback(g, ___.USELESS)
+  }
+  ___.grantRead(wave, "Mode");
+  c.prototype = wave.Callback.prototype;
+  wave.Callback.prototype.constructor = c;
+  ___.markCtor(c, Object, "Callback");
+  ___.primFreeze(c);
+  ___.tamesTo(wave.Callback, c);
+  ___.handleGenericMethod(c.prototype, "invoke", function() {
+    return ___.callPub(this.callback_, "apply", [___.tame(this.context_), Array.slice(arguments, 0)])
+  });
+  caja___.whitelistCtors([[wave, "Participant", Object], [wave, "State", Object]]);
+  caja___.whitelistMeths([[wave.Participant, "getDisplayName"], [wave.Participant, "getId"], [wave.Participant, "getThumbnailUrl"], [wave.State, "get"], [wave.State, "getKeys"], [wave.State, "reset"], [wave.State, "submitDelta"], [wave.State, "submitValue"], [wave.State, "toString"]]);
+  caja___.whitelistFuncs([[wave, "getHost"], [wave, "getMode"], [wave, "getParticipantById"], [wave, "getParticipants"], [wave, "getState"], [wave, "getTime"], [wave, "getViewer"], [wave, "isInWaveContainer"], [wave, "log"], [wave, "setModeCallback"], [wave, "setParticipantCallback"], [wave, "setStateCallback"], [wave.util, "printJson"]]);
+  b.outers.wave = ___.tame(wave);
+  ___.grantRead(b.outers, "wave")
+});wave.util = wave.util || {};
+wave.util.SPACES_ = "                                                 ";
+wave.util.toSpaces_ = function(b) {
+  return wave.util.SPACES_.substring(0, b * 2)
+};
+wave.util.isArray_ = function(b) {
+  try {
+    return b && typeof b.length == "number"
+  }catch(c) {
+    return false
+  }
+};
+wave.util.printJson = function(b, c, e) {
+  if(!b || typeof b.valueOf() != "object") {
+    if(typeof b == "string") {
+      return"'" + b + "'"
+    }else {
+      if(b instanceof Function) {
+        return"[function]"
+      }
+    }
+    return"" + b
+  }
+  var d = [], g = wave.util.isArray_(b), f = g ? "[]" : "{}", h = c ? "\n" : "", i = c ? " " : "", j = 0;
+  e = e || 1;
+  c || (e = 0);
+  d.push(f.charAt(0));
+  for(var l in b) {
+    if(!l.match(/___$/)) {
+      var k;
+      k = l;
+      var m = b[k];
+      j++ > 0 && d.push(", ");
+      if(!g) {
+        d.push(h);
+        d.push(wave.util.toSpaces_(e));
+        d.push(k + ": ");
+        d.push(i)
+      }
+      d.push(wave.util.printJson(m, c, e + 1))
+    }
+  }
+  if(!g) {
+    d.push(h);
+    d.push(wave.util.toSpaces_(e - 1))
+  }
+  d.push(f.charAt(1));
+  return d.join("")
 };wave.checkWaveContainer_ = function() {
   var b = gadgets.util.getUrlParameters();
   wave.inWaveContainer_ = b.hasOwnProperty(wave.API_PARAM_) && b[wave.API_PARAM_];
@@ -148,17 +255,17 @@ wave.receiveWaveParticipants_ = function(b) {
   wave.host_ = null;
   wave.participants_ = [];
   wave.participantMap_ = {};
-  var c = b.myId, d = b.authorId;
+  var c = b.myId, e = b.authorId;
   b = b.participants;
-  for(var e in b) {
-    if(!e.match(/___$/)) {
+  for(var d in b) {
+    if(!d.match(/___$/)) {
       var g;
-      g = e;
+      g = d;
       var f = wave.Participant.fromJson_(b[g]);
       if(g == c) {
         wave.viewer_ = f
       }
-      if(g == d) {
+      if(g == e) {
         wave.host_ = f
       }
       wave.participants_.push(f);
@@ -175,13 +282,25 @@ wave.receiveWaveParticipants_ = function(b) {
 };
 wave.receiveState_ = function(b) {
   wave.state_ = wave.state_ || new wave.State("wave_gadget_state");
+  var c = wave.state_.calculateDelta_(b);
   wave.state_.setState_(b);
-  wave.stateCallback_.invoke(wave.state_)
+  wave.stateCallback_.invoke(wave.state_, c)
 };
 wave.receivePrivateState_ = function(b) {
   wave.privateState_ = wave.privateState_ || new wave.State("wave_private_gadget_state");
+  var c = wave.privateState_.calculateDelta_(b);
   wave.privateState_.setState_(b);
-  wave.privateStateCallback_.invoke(wave.privateState_)
+  wave.privateStateCallback_.invoke(wave.privateState_, c)
+};
+wave.receiveStateDelta_ = function(b) {
+  wave.state_ = wave.state_ || new wave.State("wave_gadget_state");
+  wave.state_.applyDelta_(b);
+  wave.stateCallback_.invoke(wave.state_, b)
+};
+wave.receivePrivateStateDelta_ = function(b) {
+  wave.privateState_ = wave.privateState_ || new wave.State("wave_private_gadget_state");
+  wave.privateState_.applyDelta_(b);
+  wave.privateStateCallback_.invoke(wave.privateState_, b)
 };
 wave.receiveMode_ = function(b) {
   wave.mode_ = b || {};
@@ -220,11 +339,11 @@ wave.isPlayback = function() {
 };
 wave.setStateCallback = function(b, c) {
   wave.stateCallback_ = new wave.Callback(b, c);
-  wave.state_ && wave.stateCallback_.invoke(wave.state_)
+  wave.state_ && wave.stateCallback_.invoke(wave.state_, wave.state_.state_)
 };
 wave.setPrivateStateCallback = function(b, c) {
   wave.privateStateCallback_ = new wave.Callback(b, c);
-  wave.privateState_ && wave.privateStateCallback_.invoke(wave.privateState_)
+  wave.privateState_ && wave.privateStateCallback_.invoke(wave.privateState_, wave.privateState_.state_)
 };
 wave.setParticipantCallback = function(b, c) {
   wave.participantCallback_ = new wave.Callback(b, c);
@@ -240,6 +359,9 @@ wave.getTime = function() {
 wave.log = function(b) {
   gadgets.rpc.call(null, "wave_log", null, b || "")
 };
+wave.setSnippet = function(b) {
+  gadgets.rpc.call(null, "set_snippet", null, b || "")
+};
 wave.getWaveId = function() {
   return wave.id_
 };
@@ -248,7 +370,9 @@ wave.internalInit_ = function() {
   if(wave.isInWaveContainer()) {
     gadgets.rpc.register("wave_participants", wave.receiveWaveParticipants_);
     gadgets.rpc.register("wave_gadget_state", wave.receiveState_);
+    gadgets.rpc.register("wave_state_delta", wave.receiveStateDelta_);
     gadgets.rpc.register("wave_private_gadget_state", wave.receivePrivateState_);
+    gadgets.rpc.register("wave_private_state_delta", wave.receivePrivateStateDelta_);
     gadgets.rpc.register("wave_gadget_mode", wave.receiveMode_);
     gadgets.rpc.call(null, "wave_enable", null, "1.0")
   }
@@ -280,35 +404,30 @@ wave.ui.makeButton = function(b) {
   b.innerHTML = "<span>" + b.innerHTML + "</span>";
   b.className += " wavebutton"
 };
-wave.ui.makeDialog = function(b, c) {
+wave.ui.makeDialog = function(b, c, e) {
+  function d() {
+    b.style.display = "none"
+  }
   wave.ui.loadCss();
-  b.innerHTML = '<div class="wavedialoghead"><span>' + c + '</span></div><div class="wavedialogbody">' + b.innerHTML + "</div>";
+  var g = b.innerHTML;
+  b.innerHTML = "";
+  var f = document.createElement("div");
+  f.className = "wavedialoghead";
+  var h = document.createElement("span"), i = document.createElement("div");
+  i.className = "wavedialogclose";
+  i.onclick = e || d;
+  h.appendChild(i);
+  h.appendChild(document.createTextNode(c));
+  f.appendChild(h);
+  b.appendChild(f);
+  c = document.createElement("div");
+  c.className = "wavedialogbody";
+  c.innerHTML = g;
+  b.appendChild(c);
   b.className += " wavedialog"
 };
 wave.ui.makeFrame = function(b) {
   wave.ui.loadCss();
   b.innerHTML = '<div class="waveboxhead"><span>&nbsp;</span></div><div class="waveboxbody">' + b.innerHTML + "</div>";
   b.className += " wavebox"
-};var tamings___ = tamings___ || [], caja___, ___;
-tamings___.push(function(b) {
-  function c(d, e) {
-    var g = {apply:___.markFuncFreeze(function(f, h) {
-      return ___.callPub(d, "apply", [e, h])
-    })};
-    return new wave.Callback(g, ___.USELESS)
-  }
-  ___.grantRead(wave, "Mode");
-  c.prototype = wave.Callback.prototype;
-  wave.Callback.prototype.constructor = c;
-  ___.markCtor(c, Object, "Callback");
-  ___.primFreeze(c);
-  ___.tamesTo(wave.Callback, c);
-  ___.handleGenericMethod(c.prototype, "invoke", function() {
-    return ___.callPub(this.callback_, "apply", [___.tame(this.context_), Array.slice(arguments, 0)])
-  });
-  caja___.whitelistCtors([[wave, "Participant", Object], [wave, "State", Object]]);
-  caja___.whitelistMeths([[wave.Participant, "getDisplayName"], [wave.Participant, "getId"], [wave.Participant, "getThumbnailUrl"], [wave.State, "get"], [wave.State, "getKeys"], [wave.State, "reset"], [wave.State, "submitDelta"], [wave.State, "submitValue"], [wave.State, "toString"]]);
-  caja___.whitelistFuncs([[wave, "getHost"], [wave, "getMode"], [wave, "getParticipantById"], [wave, "getParticipants"], [wave, "getState"], [wave, "getTime"], [wave, "getViewer"], [wave, "isInWaveContainer"], [wave, "log"], [wave, "setModeCallback"], [wave, "setParticipantCallback"], [wave, "setStateCallback"], [wave.util, "printJson"]]);
-  b.outers.wave = ___.tame(wave);
-  ___.grantRead(b.outers, "wave")
-});
+};
